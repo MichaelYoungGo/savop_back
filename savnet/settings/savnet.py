f399cb6e73d5b17151bc173f9724c66be1458ae2
MichealYoungGo <ustb_yhb@163.com>
2023-03-01 15:59:07 +0800

    change database software

--- savnet/settings/savnet.py
+++ savnet/settings/savnet.py
@@ -1,8 +1,9 @@
 from .base import *
 import django.middleware.clickjacking
+import os
 
 DEBUG = True
-
+BASE_DIR = os.path.abspath(os.path.join(os.getcwd()))
 X_FRAME_OPTIONS = 'DENY'
 SECURE_CONTENT_TYPE_NOSNIFF = True
 SECURE_BROWSER_XSS_FILTER = True
@@ -19,14 +20,21 @@ REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = (
 
 ALLOWED_HOSTS = ['*']
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'postgres',
+#         'HOST': 'localhost',
+#         'PORT': 5432,
+#         'USER': 'postgres',
+#         'PASSWORD': '123qwe4r',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'HOST': 'localhost',
-        'PORT': 5432,
-        'USER': 'postgres',
-        'PASSWORD': '123qwe4r',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
