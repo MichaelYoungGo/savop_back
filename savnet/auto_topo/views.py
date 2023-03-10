79d5cd97d43a689ca3a9cc7dfc41a9801921dbc3
MichealYoungGo <ustb_yhb@163.com>
2023-03-10 16:06:57 +0800

    tmp commit

---
+++ savnet/auto_topo/views.py
@@ -0,0 +1,7 @@
+from django.forms import model_to_dict
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from constants.error_code import ErrorCode
+
+class SavnetAutoBuildTopology(APIView):
+    pass
