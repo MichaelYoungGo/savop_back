004439035c1738f3c93bbbd624e595ab73f8809f
MichealYoungGo <ustb_yhb@163.com>
2023-03-17 10:26:29 +0800

    router table 和 sav table

--- savnet/urls.py
+++ savnet/urls.py
@@ -10,6 +10,7 @@ from savnet.auto_topo.views import SavnetAutoBuildTopology
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^api/getNetData/$', log_views.FPathInfoView.as_view()),
+    url(r'^api/netinfo/refresh/(?P<topo>.+)/$', log_views.RefreshSavnetTopologyProgressData.as_view()),
     url(r'^api/netinfo/(?P<topo>.+)/$', log_views.CollectSavnetTopologyProgressData.as_view()),
     url(r'^api/auto_build/$', SavnetAutoBuildTopology.as_view()),
 ]
