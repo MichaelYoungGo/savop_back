015af9624a8bc59b691546330b28cdac7c7182f0
MichaelYoungGo <ustb_yhb@163.com>
2023-03-24 10:52:56 +0800

    adjust peoject dir

--- gunicorn.py
+++ gunicorn.py
@@ -1,8 +1,8 @@
-chdir = '/root/savnet_back/savnet'
+chdir = '/root/savop_back/savnet'
 workers = 1
 threads = 1
 bind = '0.0.0.0:5000'
 daemon = 'false'
 worker_connections = 2000
-pidfile = '/root/savnet_back/log/gunicorn.pid'
+pidfile = '/root/savop_back/log/gunicorn.pid'
 loglevel = 'debug' 
