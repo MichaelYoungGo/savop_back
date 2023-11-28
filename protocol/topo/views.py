77dd3095137b59f6f5e16940dd58c0de59dfbd78
MichaelYoungGo <ustb_yhb@163.com>
2023-11-28 11:06:22 +0800

    生成描述任意拓扑的Json文件

--- protocol/topo/views.py
+++ protocol/topo/views.py
@@ -10,6 +10,8 @@
                    2023/6/25:
 -------------------------------------------------
 """
+import json
+
 import pymongo
 import time
 from rest_framework.viewsets import ViewSet
@@ -150,6 +152,52 @@ class TopologySet(ViewSet):
         MongoDBClient.delete_by_id(id_=params)
         return response_data(data="delete, successfully!")
 
-
-
-
+    @action(detail=False, methods=['get'], url_path="config", url_name="delete_topo")
+    def config(self, request, *args, **kwargs):
+        params = request.query_params.get("id")
+        if params is None:
+            return response_data(code=ErrorCode.E_PARAM_ERROR, message="Request parameter can not be empty. Please checkout your request!")
+        if MongoDBClient.exists_by_id(id_=params) is False:
+            return response_data(code=ErrorCode.E_PARAM_ERROR, message="the config topology don't existed")
+        # 将数据库中的数据转化为纯数据文件
+        data = MongoDBClient.find_one_by_id(id_=params)[0]
+        topo_name = data["name"]
+        data = data["data"]["content"]
+        config_file = {"devices": {}, "links": [], "enable_rpki": False, "prefix_method": "blackhole", "ip_version": 4}
+        # 将纯数据文件转化为描述任意topo结构的json文件
+        # 首先提取topo的各种构件信息
+        as_info = {}
+        prefix_info = {}
+        router_info = {}
+        for component in data["nodes"]:
+            component_type = component["id"].split("_")[0]
+            if component_type == "as":
+                as_id = component["id"]
+                label = component["label"]
+                as_info[as_id] = label
+            elif component_type == "pref":
+                router_id = component["businessInfo"]["affiliationRouter"]
+                prefix_IPs = component["businessInfo"]["prefixIP"]
+                prefix_IP_list = prefix_IPs.split(",")
+                prefix_info.update({router_id: prefix_IP_list})
+            elif component_type == "rt":
+                router_mongo_id = component["id"]
+                router_id = component["businessInfo"]["routerId"]
+                router_info.update({router_mongo_id: router_id})
+        for component in data["nodes"]:
+            component_type = component["id"].split("_")[0]
+            if component_type == "rt":
+                as_id = component["businessInfo"]["affiliationAS"]
+                router_id = component["businessInfo"]["routerId"]
+                router_mongo_id = component["id"]
+                config_file["devices"].update({router_id: {"as": as_info[as_id], "prefixes": prefix_info[router_mongo_id]}})
+        for component in data["edges"]:
+            source_router_mongo_id = component["source"]
+            target_router_mongo_id = component["target"]
+            source_router_id = router_info[source_router_mongo_id]
+            target_router_id = router_info[target_router_mongo_id]
+            config_file["links"].append([source_router_id, target_router_id, "dsav"])
+        # 打开文件以进行写入，如果文件不存在则创建
+        with open(f'/root/sav_simulate/sav-start/base_configs/{topo_name}.json', 'w') as file:
+            file.write(json.dumps(config_file, indent=2))
+        return response_data(data=config_file)
