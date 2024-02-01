392b8b1d3b9e5574de88402a6d23836616c55634
MichaelYoungGo <ustb_yhb@163.com>
2024-02-01 11:00:29 +0800

    store the config files of supervisord and gunicorn

--- gunicorn.py
+++ gunicorn.py
@@ -1,8 +1,8 @@
-chdir = '/root/savop_back/protocol'
+chdir = '/root/sav_simulate/savop_back/protocol'
 workers = 1
 threads = 1
 bind = '0.0.0.0:5000'
 daemon = 'false'
 worker_connections = 2000
-pidfile = '/root/savop_back/log/gunicorn.pid'
+pidfile = '/root/sav_simulate/savop_back/log/gunicorn.pid'
 loglevel = 'debug' 
